2be79243db9b1835646016af918f5f8e6a6e61a6
Josh van Eyken <josh.vaneyken@gmail.com>
2023-11-01 18:19:32 -0700

    Fill out Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,18 @@ function Card() {
     return(
         <>
             I am a Card component
+            <div>Front of Card</div>
+            <div>Word</div>
+
+            <div>Back of Card</div>
+            <div>Translation</div>
+            <div>Did you get it right?</div>
+            <label>Yes
+                <input type="checkbox" />
+            </label>
+            <label>No
+                <input type="checkbox" />
+            </label>
         </>
     )
 }
